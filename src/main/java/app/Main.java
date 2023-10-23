ffd2c3115f18b77bc23b858068bea33490da649e
Mattsigurd <124781100+Mattsigurd@users.noreply.github.com>
2023-10-23 09:36:59 +0100

    Add files via upload

---
+++ src/main/java/app/Main.java
@@ -0,0 +1,22 @@
+package app;
+
+import config.ThymeleafConfig;
+import io.javalin.Javalin;
+import io.javalin.rendering.template.JavalinThymeleaf;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+        // Initializing Javalin and Jetty webserver
+
+        Javalin app = Javalin.create(config -> {
+            config.staticFiles.add("/public");
+            JavalinThymeleaf.init(ThymeleafConfig.templateEngine());
+        }).start(7080);
+
+        // Routing
+
+        app.get("/", ctx -> ctx.render("index.html"));
+    }
+}
