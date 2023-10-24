ff4864ec382033f53c71487993be29e02eeed5f7
MGold <mikkelgold@protonmail.com>
2023-10-24 09:02:40 +0200

    updated lidt at det hele så DB burde virke

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -1,10 +1,20 @@
 package app;
 
+import app.controllers.UserController;
+import app.persistence.ConnectionPool;
 import config.ThymeleafConfig;
 import io.javalin.Javalin;
 import io.javalin.rendering.template.JavalinThymeleaf;
 
 public class Main {
+
+    private static final String USER = "postgres";
+    private static final String PASSWORD = "postgres";
+    private static final String URL = "jdbc:postgresql://localhost:5432/%s?currentSchema=public";
+    private static final String DB = "cupcake";
+
+    private static final ConnectionPool connectionPool = ConnectionPool.getInstance(USER, PASSWORD, URL, DB);
+
 
 
     public static void main(String[] args) {
@@ -13,10 +23,14 @@ public class Main {
         Javalin app = Javalin.create(config -> {
             config.staticFiles.add("/public");
             JavalinThymeleaf.init(ThymeleafConfig.templateEngine());
-        }).start(7090);
+        }).start(7070);
 
         // Routing
 
         app.get("/", ctx -> ctx.render("index.html"));
+        app.post("/login", ctx -> UserController.login(ctx, connectionPool));
+        app.get("/createuser", ctx -> ctx.render("createuser.html"));
+        app.post("/createuser",ctx -> UserController.createuser(ctx, connectionPool ));
+        app.get("/logout", ctx -> UserController.logout(ctx));
     }
 }
