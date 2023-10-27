810f935386b716a8757506e4a494e230ed4970f7
Yolo-83 <cph-yc123@cphbusiness.dk>
2023-10-27 10:14:50 +0200

    .

--- src/main/java/app/entities/User.java
+++ src/main/java/app/entities/User.java
@@ -1,5 +1,13 @@
 package app.entities;
 
+import app.exceptions.DatabaseException;
+import app.persistence.ConnectionPool;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class User
 {
     private int id;
@@ -23,18 +31,15 @@ public class User
         this.password = password;
     }
 
-    public int getId()
-    {
+    public int getId() {
         return id;
     }
 
-    public String getEmail()
-    {
+    public String getEmail() {
         return email;
     }
 
-    public String getPassword()
-    {
+    public String getPassword() {
         return password;
     }
 
@@ -54,4 +59,25 @@ public class User
                 ", balance=" + balance +
                 '}';
     }
+
+
+
+    public static String findAdministratorByRole(String userId, ConnectionPool connectionPool) throws DatabaseException {
+        String sql = "SELECT order_id FROM user_orders WHERE user_id = ?";
+        try (Connection connection = connectionPool.getConnection()) {
+            try (PreparedStatement ps = connection.prepareStatement(sql)) {
+                ps.setString(1, userId);
+                try (ResultSet rs = ps.executeQuery()) {
+                    if (rs.next()) {
+                        return rs.getString("role");
+                    } else {
+                        throw new DatabaseException("Ingen role fundet med brugeren " + userId);
+                    }
+                }
+            }
+        } catch (SQLException e) {
+            throw new DatabaseException("Kan ikke connecte til DB i findAdministratorByRole: " + e.getMessage());
+        }
+    }
+
 }
