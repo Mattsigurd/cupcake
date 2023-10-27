e60067c5a0fbfce53b5c0746192455f4ab2e52ce
Yolo-83 <cph-yc123@cphbusiness.dk>
2023-10-27 11:07:54 +0200

    .

--- src/main/java/app/entities/User.java
+++ src/main/java/app/entities/User.java
@@ -1,5 +1,13 @@
 package app.entities;
 
+import app.exceptions.DatabaseException;
+import app.persistence.ConnectionPool;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class User
 {
     private int id;
@@ -60,4 +68,26 @@ public class User
                 ", balance=" + balance +
                 '}';
     }
+
+
+    /*
+    public static String findOrderIdByRole(String userId, ConnectionPool connectionPool) throws DatabaseException {
+        String sql = "SELECT order_id FROM orders WHERE user_id = ?";
+        try (Connection connection = connectionPool.getConnection()) {
+            try (PreparedStatement ps = connection.prepareStatement(sql)) {
+                ps.setInt(1, findOrderIdByRole());
+                try (ResultSet rs = ps.executeQuery()) {
+                    if (rs.next()) {
+                        return rs.getString("order_id");
+                    } else {
+                        throw new DatabaseException("Ingen order fundet med brugeren " + userId);
+                    }
+                }
+            }
+        } catch (SQLException e) {
+            throw new DatabaseException("Kan ikke connecte til DB i findOrderIdByRole: " + e.getMessage());
+        }
+    }
+
+     */
 }
