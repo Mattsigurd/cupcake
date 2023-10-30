d4e249a3b79796033af79837e39c23cb9a282e33
MGold <mikkelgold@protonmail.com>
2023-10-30 11:37:17 +0100

    update

--- src/main/java/app/controllers/OrderController.java
+++ src/main/java/app/controllers/OrderController.java
@@ -14,6 +14,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static app.persistence.OrderMapper.findOrderIdByUserId;
+import static app.persistence.OrderMapper.findUserIdByCurrentUser;
 
 
 public class OrderController {
